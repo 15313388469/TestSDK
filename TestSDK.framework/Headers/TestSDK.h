5f2225ee2ab8791935fdd98403b11a070f2feede
lilixi <15313388469@users.noreply.github.com>
2023-01-11 17:29:09 +0800

    Add files via upload
    首次上传

---
+++ TestSDK.framework/Headers/TestSDK.h
@@ -0,0 +1,18 @@
+//
+//  TestSDK.h
+//  UnicomPay
+//
+//  Created by 江星 on 2023/1/11.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestSDK : NSObject
+
++ (void)hello;
+
+@end
+
+NS_ASSUME_NONNULL_END
